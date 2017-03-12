8959bbb6898a0ad39b76ac3cfdff453e502398ee
shafjl4 <Shafjl4@gmail.com>
2017-03-12 10:27:04 -0400

    start level1

--- client/sidebar.js
+++ client/sidebar.js
@@ -1,4 +1,8 @@
 Template.sidebar.events({
+  'click .level1': function (evt) {
+    Session.set('context', 'Providers');
+    Session.set('search', '[a-zA-Z]+')
+  },
   'click .providers': function (evt) {
     Session.set('context', 'Providers');
     Session.set('search', '[a-zA-Z]+')
@@ -7,11 +11,11 @@ Template.sidebar.events({
     Session.set('context', 'Techs');
     Session.set('search', '[a-zA-Z]+')
   },
-  'click #section-title': function (evt) {
-  	Session.set('context', '[a-zA-Z]+')
-  },
   'click .ed-providers': function (evt) {
     Session.set('context', 'EDProviders');
     Session.set('search', '[a-zA-Z]+')
+  },
+  'click #section-title': function (evt) {
+  	Session.set('context', '[a-zA-Z]+')
   }
 });
