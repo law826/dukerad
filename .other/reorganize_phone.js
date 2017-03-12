c6b612b771a9faf14819c554eae1119d034f84e9
shafjl4 <Shafjl4@gmail.com>
2017-03-12 09:52:18 -0400

    update the reorganize phone script

--- .other/reorganize_phone.js
+++ .other/reorganize_phone.js
@@ -1,19 +1,28 @@
+// Run this after importing the TSV from google spreadsheets into collection tmppn.
+// Level 1 is the first level on the sidebar outline, e.g., Providers, Reading Rooms.
+// Level 2 is the second level in the sidebar outline. 
+// Level 3 is the level in which is a box is formed on the main panel. 
+// There are going to be some levels in which the Level 2 and Level 3 are redundant, e.g., the Reading Rooms.
+
 db.tmppn.find().addOption(DBQuery.Option.noTimeout).forEach(
     function(pn_doc) {
-    	// var existing_location_document = db.phoneNumbers.findOne({location: pn_doc.location, type: pn_doc.type, misc: pn_doc.misc})
-        var existing_location_document = db.phoneNumbers.findOne({location: pn_doc.location})
-    	if (existing_location_document == null) {
+    	// var existing_level_3_document = db.phoneNumbers.findOne({level_3: pn_doc.level_3, level_1: pn_doc.level_1, misc: pn_doc.misc})
+        // If the level 3 document does not yet exist. 
+        var existing_level_3_document = db.phoneNumbers.findOne({level_3: pn_doc.level_3})
+    	if (existing_level_3_document == null) {
     		db.phoneNumbers.insert({
-    			location: pn_doc.location,
-    			type: pn_doc.type,
+                level_1: pn_doc.level_1,
+                level_2: pn_doc.level_2,
+    			level_3: pn_doc.level_3,
     			numbers: [{person: pn_doc.person, number: pn_doc.number}],
     			misc: pn_doc.misc
     		})
     	} else {
-    		var numbers_array = existing_location_document.numbers
+            // If the level 3 document already exists. 
+    		var numbers_array = existing_level_3_document.numbers
     		var new_array_entry = {person: pn_doc.person, number: pn_doc.number}
     		numbers_array.push(new_array_entry)
-    		db.phoneNumbers.update({_id: existing_location_document._id},
+    		db.phoneNumbers.update({_id: existing_level_3_document._id},
     								{$set: {numbers: numbers_array}})
     	}
     }
